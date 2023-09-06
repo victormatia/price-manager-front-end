52edf907f51e4301961718142644bcc923b8a35a
victormatia <fvictor1705@gmail.com>
2023-09-06 18:38:32 -0300

    feat: implements ActionSection in the Home page

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,9 +1,11 @@
+import ActionSection from '@/components/ActionSection';
 import Upload from '@/components/Upload';
 
 export default function Home() {
   return (
     <main>
       <Upload/>
+      <ActionSection />
     </main>
   );
 }
