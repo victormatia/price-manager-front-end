2d794692b857bea3f77c8345a2bf924ad9095d34
victormatia <fvictor1705@gmail.com>
2023-09-08 00:59:14 -0300

    feat: implements ErrorList component in the Home page

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,10 +1,12 @@
 import ActionSection from '@/components/ActionSection';
+import ErrorsList from '@/components/ErrorsList';
 import Upload from '@/components/Upload';
 
 export default function Home() {
   return (
     <main>
       <Upload/>
+      <ErrorsList />
       <ActionSection />
     </main>
   );
