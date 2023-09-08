788cc73fbd245112094e1e825efa7c848a486a1f
victormatia <fvictor1705@gmail.com>
2023-09-08 00:57:44 -0300

    feat: adds errorList and isUpdateEnabled states in the provider

--- src/context/Provider.tsx
+++ src/context/Provider.tsx
@@ -1,14 +1,20 @@
 import { ReactNode, useMemo, useState } from 'react';
 import { FileWithPath } from 'react-dropzone';
 
+import TInvalidPrices from 'src/types/TInvalidPrice';
+
 import context from './context';
 
 export default function Provider({ children }: { children: ReactNode }) {
   const [file, setFile] = useState<FileWithPath | null>(null);
+  const [errorList, setErrorList] = useState<TInvalidPrices[]>([]);
+  const [isUpdateEnabled, setIsUpdateEnabled] = useState<boolean>(false);
 
   const states = useMemo(() => ({
     file, setFile,
-  }), [file]);
+    errorList, setErrorList,
+    isUpdateEnabled, setIsUpdateEnabled,
+  }), [file, errorList, isUpdateEnabled]);
 
   return (
     <context.Provider value={states}>
