c271d43960b0a52fad455225b6f83c4d17b1d025
victormatia <fvictor1705@gmail.com>
2023-09-08 01:05:46 -0300

    feat: enable the update button when no price is invalid and set all error messages otherwise

--- src/components/ActionSection/index.tsx
+++ src/components/ActionSection/index.tsx
@@ -9,21 +9,26 @@ import Button from '../Button';
 const VALIDATE_URL = 'http://localhost:3001/validate';
 
 export default function ActionSection() {
-  const { file } = useContext(context);
+  const { file, setErrorList, isUpdateEnabled, setIsUpdateEnabled } = useContext(context);
 
   const onSubmit = async () => {
     const data = new FormData();
 
     data.append('file', file as Blob);
 
-    const response = await requestFunction(VALIDATE_URL, data);
-    console.log(response);
+    const areInvalidPrices = await requestFunction(VALIDATE_URL, data);
+
+    areInvalidPrices.forEach((e: any) => {
+      if (!e.isValid) setErrorList((prev) => [...prev, e]);
+    });
+
+    setIsUpdateEnabled(areInvalidPrices.every((e: any) => e.isValid));
   };
   
   return (
     <section>
       <Button title='Validar' type='button' action={ onSubmit } isDisabled={ false } />
-      <Button title='Atualizar' type='button' action={ () => {} } isDisabled={ true } />
+      <Button title='Atualizar' type='button' action={ () => {} } isDisabled={ !isUpdateEnabled } />
     </section>
   );
 }
