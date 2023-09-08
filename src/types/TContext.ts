44dc851c5ac0a46e0560eb5ef07a931d8a052f74
victormatia <fvictor1705@gmail.com>
2023-09-08 18:47:39 -0300

    chore: adds updatedProducts and setUpdatesProducts types in the TContext

--- src/types/TContext.ts
+++ src/types/TContext.ts
@@ -2,6 +2,7 @@ import { Dispatch, SetStateAction } from 'react';
 import { FileWithPath } from 'react-dropzone';
 
 import TInvalidPrices from './TInvalidPrice';
+import TProduct from './TProduct';
 
 type TContext = {
   file: FileWithPath | null,
@@ -10,6 +11,8 @@ type TContext = {
   setErrorList: Dispatch<SetStateAction<TInvalidPrices[]>>,
   isUpdateEnabled: boolean,
   setIsUpdateEnabled: Dispatch<SetStateAction<boolean>>,
+  updatedProducts: TProduct[],
+  setUpdatesProducts: Dispatch<SetStateAction<TProduct[]>>
 }
 
 export default TContext;
