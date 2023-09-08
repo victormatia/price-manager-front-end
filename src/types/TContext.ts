f14378e5225b7b9617b618b55e62029eeb640ae1
victormatia <fvictor1705@gmail.com>
2023-09-08 00:55:42 -0300

    chore: adds  errorList, setErrorList, isUpdateEnabled, setIsUpdateEnabled types in the TContext

--- src/types/TContext.ts
+++ src/types/TContext.ts
@@ -1,9 +1,15 @@
 import { Dispatch, SetStateAction } from 'react';
 import { FileWithPath } from 'react-dropzone';
 
+import TInvalidPrices from './TInvalidPrice';
+
 type TContext = {
-  file: FileWithPath | null
-  setFile: Dispatch<SetStateAction<FileWithPath | null>>
+  file: FileWithPath | null,
+  setFile: Dispatch<SetStateAction<FileWithPath | null>>,
+  errorList: TInvalidPrices[], 
+  setErrorList: Dispatch<SetStateAction<TInvalidPrices[]>>,
+  isUpdateEnabled: boolean,
+  setIsUpdateEnabled: Dispatch<SetStateAction<boolean>>,
 }
 
 export default TContext;
