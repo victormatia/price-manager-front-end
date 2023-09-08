47738fb0d1e69d3b711328d4b23c8633184ad2ca
victormatia <fvictor1705@gmail.com>
2023-09-08 18:49:13 -0300

    feat: adds updatedProducts state in the Provider

--- src/context/Provider.tsx
+++ src/context/Provider.tsx
@@ -2,6 +2,7 @@ import { ReactNode, useMemo, useState } from 'react';
 import { FileWithPath } from 'react-dropzone';
 
 import TInvalidPrices from 'src/types/TInvalidPrice';
+import TProduct from 'src/types/TProduct';
 
 import context from './context';
 
@@ -9,12 +10,14 @@ export default function Provider({ children }: { children: ReactNode }) {
   const [file, setFile] = useState<FileWithPath | null>(null);
   const [errorList, setErrorList] = useState<TInvalidPrices[]>([]);
   const [isUpdateEnabled, setIsUpdateEnabled] = useState<boolean>(false);
+  const [updatedProducts, setUpdatesProducts] = useState<TProduct[]>([]);
 
   const states = useMemo(() => ({
     file, setFile,
     errorList, setErrorList,
     isUpdateEnabled, setIsUpdateEnabled,
-  }), [file, errorList, isUpdateEnabled]);
+    updatedProducts, setUpdatesProducts,
+  }), [file, errorList, isUpdateEnabled, updatedProducts]);
 
   return (
     <context.Provider value={states}>
