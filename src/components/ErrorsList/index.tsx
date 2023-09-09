bfb1ff7fed5befbb7e6289964866938756ab2e77
victormatia <fvictor1705@gmail.com>
2023-09-08 23:06:15 -0300

    style: styles ErrorList component

--- src/components/ErrorsList/index.tsx
+++ src/components/ErrorsList/index.tsx
@@ -3,6 +3,7 @@ import { useContext } from 'react';
 import context from 'src/context/context';
 
 import Message from '../Message';
+import style from './style.module.css';
 
 export default function ErrorsList() {
   const { errorList } = useContext(context);
@@ -10,9 +11,9 @@ export default function ErrorsList() {
     <>
       {
         !!errorList.length && (
-          <ul>
+          <ul className={ style['error-list'] }>
             { errorList.map((e, i) => (
-              <li key={ e.productCode + i }>
+              <li className={ style['error-list__item'] } key={ e.productCode + i }>
                 <Message  message={
                   `Product code: ${e.productCode} - Error Message: ${e.message}`
                 } />
