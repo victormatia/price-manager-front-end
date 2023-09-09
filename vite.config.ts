17da5c3b07462009996ef9460cec148ce46be251
victormatia <fvictor1705@gmail.com>
2023-09-09 03:17:06 -0300

    build: setup server to vite.config

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,11 @@ import { defineConfig } from 'vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
+  server: {
+    host: true,
+    strictPort: true,
+    port: 3000,
+  },
   plugins: [
     react(), 
     tsconfigPaths(), // esse plugin resolve o problema do paths mapping do vite
