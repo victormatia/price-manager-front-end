de9cd727fce36e01b842f3ae22b7bf509d20ab93
victormatia <fvictor1705@gmail.com>
2023-09-08 23:20:18 -0300

    style: styles TableRow component

--- src/components/TableRow/index.tsx
+++ src/components/TableRow/index.tsx
@@ -1,8 +1,10 @@
 import TProduct from 'src/types/TProduct';
 
+import style from './style.module.css';
+
 export default function TableRow({ code, cost_price, name, new_sales_price, sales_price }: TProduct) {
   return (
-    <tr>
+    <tr className={ style['row'] }>
       <td>{ code }</td>
       <td>{ name }</td>
       <td>{ cost_price }</td>
